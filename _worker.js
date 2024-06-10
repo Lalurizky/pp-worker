caee4aa37e391cca3a676d9ae67ee255f3a8ba60
Lalurizky <137436584+Lalurizky@users.noreply.github.com>
2024-06-11 05:47:38 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ import { connect } from 'cloudflare:sockets';
 // t.me/P_tech2024 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = 'fa403c2b-7ed4-49f6-814a-e931323a2a6d';
+let userID = '2cd61524-9843-43ab-8a0a-df4a744bb859';
 
 const proxyIPs = ['mtn.ircf.space', 'mkh.ircf.space', 'mci.ircf.space', 'rtl.ircf.space'];
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
